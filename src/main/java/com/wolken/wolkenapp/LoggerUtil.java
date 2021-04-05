636e2b3675c35cd045d0fdf6cbbc299e22ad2110
Kishan G <kishan@wolkensoftware.com>
2021-04-05 23:12:53 +0530

    Adding: demo of loggers

---
+++ src/main/java/com/wolken/wolkenapp/LoggerUtil.java
@@ -0,0 +1,16 @@
+package com.wolken.wolkenapp;
+
+import org.apache.log4j.Logger;
+
+public class LoggerUtil {
+	public static void main(String[] args) {
+		Logger logger=Logger.getLogger("LoggerUtil");
+		logger.debug("sahgfhd");
+		logger.error("zjhgfasj");
+		logger.fatal("xzljdfsa");
+		logger.info("sajhgdhj");
+		logger.warn("dfghihbjkg");
+		
+	}
+
+}
